62bd14fc1196c81ff8e84ae53369041bff5a7584
WilliamFalasca <102008572+WilliamFalasca@users.noreply.github.com>
2022-03-21 01:02:11 -0300

    Adicionados os arquivos do portfolio
    Portfólio treinamento Imersão Dev Alura 08 a 20/03/2022

---
+++ script.js
@@ -0,0 +1,29 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
+
+//FIGMA AULA 9 - https://www.figma.com/file/1flmz2iauuNs8JsY6eaBHI/Imers%C3%A3o-Dev---Aula-9?node-id=53%3A2
+
+//FIGMA AULA 10 - https://www.figma.com/file/I4p9ndfOKgjDKalFwd5ASr/Imers%C3%A3o-Dev---Aula-10?node-id=0%3A1
+
+//h1 no HTML é geralmente o título principal
+
+//fontes de texto podem ser encontradas em google fonts
+
+//.container pega todos os tipos que tiverem class container, não só header
+
+//padding é semelahnte ao margin, mas da contorno a elementos mais internos como a foto e os textos
+
+//<ul> são listas não ordenadas e <ol> são ordenadas
+
+//<a> é tag ancora que clica nela para abrir outra coisa
+
+//emojis em texto -> https://emojipedia.org/search/?q=folder
+
+//list style: none para tirar os icones de lista
+
+//text-decoration none para tirar sublinhado
+
+//embed dos projetos pode ser feito com vídeos do YT, posts do insta, etc
+
+//padding: 8px 16px; primeiro numero é na vertical e o segund na horizontal (distancia borda ao texto)
